07bcf62e212ee9371cb2fd6a018d24666db009e8
lu2311 <luisyraita2306@gmail.com>
2025-12-05 09:41:08 -0500

    Cambio de enlace en axios

--- src/services/TaskService.jsx
+++ src/services/TaskService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/tareas"; // <-- ajusta a tu ruta
+const API_URL = "https://todo-backend-zux0.onrender.com"; // <-- ajusta a tu ruta
 
 export const getTasks = () => axios.get(API_URL);
 
